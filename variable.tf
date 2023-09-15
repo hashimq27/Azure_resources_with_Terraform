bcf67c94def03d3f6aa8c3ae321d798b9cd6676a
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-09-14 21:50:47 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,15 @@
 variable "subscription_id"{
-  
+  type=string
 }
 variable "client_id"{
+  type=string
   
 }
 variable "client_secret"{
+  type=string
   
 }
 variable "teneant_id"{
+  type=string
   
 }
