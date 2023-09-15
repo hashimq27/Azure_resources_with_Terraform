763ec59550b7be11fc658bd6c488fc51ba751c3a
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-09-14 22:03:42 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,6 @@ provider "azurerm"{
   skip_provider_registration="true"
   subscription_id=var.subscription_id
   client_id=var.client_id
-  Client_secret=var.client_secret
+  client_secret=var.client_secret
   tenant_id=var.tenant_id
 }
