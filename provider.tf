24bfd6e8aa116c32532ac659a20fc02da50aba10
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-09-14 20:46:57 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers{
+    azurerm={
+      source="hashicorp/terrafrom/azurerm"
+      version=">3.70.0"
+    }
+  }
+  required_version="1.4.0"
+}
+
+provider "azurerm"{
+  features{}
+  skip_provider_registration="true"
+  
+}
