66a6a3c31d9485590fa7cd526c41122e8f48f381
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-09-16 19:41:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "batch06"{
 
 
 resource "azurerm_storage_account" "awp" {
-  name                     = "storageaccountname"
+  name                     = "${var.prefix}storage${var.env}"
   resource_group_name      = azurerm_resource_group.batch06.name
   location                 = azurerm_resource_group.batch06.location
   account_tier             = "Standard"
