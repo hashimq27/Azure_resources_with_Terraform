de5593f7cfe4789685b330fac0ea8788ce93fff9
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-09-21 20:39:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,3 +61,21 @@ output "kube_config" {
 
   sensitive = true
 }
+
+resource "azurerm_public_ip" "publicIP" {
+  name                = var.ip_name
+  location            = azurerm_resource_group.batch06.location
+  resource_group_name = azurerm_resource_group.batch06.name
+  allocation_method   = var.method_allocation
+}
+
+resource "azurerm_lb" "loadbalancer" {
+  name                = var.lb_name
+  location            = azurerm_resource_group.batch06.location
+  resource_group_name = azurerm_resource_group.batch06.name
+
+  frontend_ip_configuration {
+    name                 = var.ipconfig_name
+    public_ip_address_id = azurerm_public_ip.publicIP.id
+  }
+}
