53a3338f5e700f18fc0cb571454e276662027b6a
hashimq27 <hashim.qasim@outlook.com>
2023-09-25 23:02:09 -0400

    comment

--- sql.tf
+++ sql.tf
@@ -2,6 +2,19 @@ resource "azurerm_postgresql_server" "sqlserver" {
   name                = "postgresql-server-1"
   location            = azurerm_resource_group.batch06.location
   resource_group_name = azurerm_resource_group.batch06.name
+
+  sku_name = "B_Gen5_2"
+
+  storage_profile {
+    storage_mb            = 5120
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+
+  administrator_login          = "psqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement              = "Enabled"
 }
 
 resource "azurerm_postgresql_database" "sqldb" {
