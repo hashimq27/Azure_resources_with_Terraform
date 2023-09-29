da7895d9951038e9421794290d3f41c824e7c969
hashimq27 <hashim.qasim@outlook.com>
2023-09-29 01:59:28 -0400

    c

--- kubecluster.tf
+++ kubecluster.tf
@@ -1,7 +1,7 @@
 resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
- for_each               = {for cluster in azurerm_kubernetes_cluster.kube1: cluster.name=>cluster.id}
+ for_each               = azurerm_kubernetes_cluster.kube1
  name                   = "${each.key}"
- kubernetes_cluster_id  = tostring(each.value)
+ kubernetes_cluster_id  = each.value.id
  vm_size                = "Standard_DS2_v2"
  node_count             = 1
 
