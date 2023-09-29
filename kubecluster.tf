1670c4b30203b15e2e7ff6b0fa7057c63bf9ba42
hashimq27 <hashim.qasim@outlook.com>
2023-09-28 22:05:54 -0400

    commit

--- kubecluster.tf
+++ kubecluster.tf
@@ -1,6 +1,7 @@
 resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
- name                  = "internal"
- kubernetes_cluster_id = [for cluster in azurerm_kubernetes_cluster.kube1: cluster.id]
+ name                   = "internal"
+ for_each               = {for cluster in local.student_names: cluster=>cluster}
+ kubernetes_cluster_id  = each.key
   vm_size               = "Standard_DS2_v2"
   node_count            = 1
 
