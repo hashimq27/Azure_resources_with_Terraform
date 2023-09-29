eefa24efad0a990cbb776ed55dae87d482c5cc0b
hashimq27 <hashim.qasim@outlook.com>
2023-09-28 22:02:49 -0400

    commit

--- kubecluster.tf
+++ kubecluster.tf
@@ -1,10 +1,10 @@
-#resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
-# name                  = "internal"
-# kubernetes_cluster_id = azurerm_kubernetes_cluster.kube1.id
-#  vm_size               = "Standard_DS2_v2"
-#  node_count            = 1
-#
-#  tags = {
-#    Environment = "Production"
-#  }
-#}
+resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
+ name                  = "internal"
+ kubernetes_cluster_id = [for cluster in azurerm_kubernetes_cluster.kube1: cluster.id]
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
