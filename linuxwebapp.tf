df06fbe0b903e317ec5ff40b24a5de52733d3cf5
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 15:33:19 -0400

    c

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -1,8 +1,8 @@
 resource "azurerm_service_plan" "servplan" {
   for_each            = {for x in local.deployment: x=> x}
   name                = each.key
-  resource_group_name = azurerm_resource_group.batch06.name[each.key]
-  location            = azurerm_resource_group.batch06.location[each.key]
+  resource_group_name = azurerm_resource_group.batch06.name
+  location            = azurerm_resource_group.batch06.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
