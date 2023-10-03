85a21701f95869e90b563759d799f501cd4c7729
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 15:46:04 -0400

    45

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -11,8 +11,8 @@ resource "azurerm_linux_web_app" "linwebapp" {
   for_each            = {for x in local.deployment: x=> x}
   name                = each.key
   resource_group_name = azurerm_resource_group.batch06.name
-  location            = azurerm_service_plan.servplan.[each.key]
-  service_plan_id     = azurerm_service_plan.servplan.[each.key]
+  location            = azurerm_service_plan.servplan[each.value]
+  service_plan_id     = azurerm_service_plan.servplan[each.value]
 
   site_config {}
 }
