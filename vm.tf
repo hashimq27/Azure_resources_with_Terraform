f4a364bd4ca6af7a031b5a0585b3db048e8dd4ea
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 16:22:41 -0400

    47

--- vm.tf
+++ vm.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.batch06.name
-  virtual_network_name = azurerm_virtual_network.vm.name
+  virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
