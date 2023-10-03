cb6e080944613f4b9d7e843d23a76b71af7dfb3a
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 16:33:20 -0400

    48

--- vm.tf
+++ vm.tf
@@ -22,7 +22,7 @@ resource "azurerm_virtual_machine" "main" {
   name                  = each.key
   location              = azurerm_resource_group.batch06.location
   resource_group_name   = azurerm_resource_group.batch06.name
-  network_interface_ids = "${azurerm_network_interface.main[each.value].id}"
+  network_interface_ids = [azurerm_network_interface.main[each.value].id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
