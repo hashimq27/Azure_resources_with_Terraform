a8f6ef3f940eb81a78947ee0181926df49ab691a
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 15:31:21 -0400

    c

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -1,8 +1,8 @@
 resource "azurerm_service_plan" "servplan" {
   for_each            = {for x in local.deployment: x=> x}
   name                = each.key
-  resource_group_name = azurerm_resource_group.batch06.name
-  location            = azurerm_resource_group.batch06.location
+  resource_group_name = azurerm_resource_group.batch06.name[each.key]
+  location            = azurerm_resource_group.batch06.location[each.key]
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
@@ -11,8 +11,8 @@ resource "azurerm_linux_web_app" "linwebapp" {
   for_each            = {for x in local.deployment: x=> x}
   name                = each.key
   resource_group_name = azurerm_resource_group.batch06.name
-  location            = azurerm_service_plan.servplan.location
-  service_plan_id     = azurerm_service_plan.servplan.id
+  location            = azurerm_service_plan.servplan[each.key]
+  service_plan_id     = azurerm_service_plan.servplan[each.key]
 
   site_config {}
 }
