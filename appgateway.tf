e81066614bac87b22b1ea154c92c1285bf43dc74
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 21:56:02 -0400

    63343

--- appgateway.tf
+++ appgateway.tf
@@ -40,7 +40,7 @@ locals {
 resource "azurerm_application_gateway" "network" {
   for_each            = azurerm_firewall_policy.firepolicy
   firewall_policy_id  = "${each.key}"
-  name                = "${each.key}"
+  name                = "${var.prefix}-${each.key}"
   resource_group_name = azurerm_resource_group.batch06.name
   location            = azurerm_resource_group.batch06.location
 
