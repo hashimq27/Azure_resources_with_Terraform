4ae239955227b685ca4e5101471110ffbb58eee5
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 20:58:50 -0400

    468

--- appgateway.tf
+++ appgateway.tf
@@ -78,7 +78,6 @@ resource "azurerm_application_gateway" "network" {
   }
 
   http_listener {
-    for_each                       = azurerm_application_gateway.network
     name                           = each.value.listener_name
     frontend_ip_configuration_name = each.value.frontend_ip_configuration_name
     frontend_port_name             = each.value.frontend_port_name
