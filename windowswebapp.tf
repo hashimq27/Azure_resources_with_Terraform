a384514c94608c00e52c05e1f5e381cef6676f97
hashimq27 <hashim.qasim@outlook.com>
2023-10-03 22:27:06 -0400

    sfgdfsag

--- windowswebapp.tf
+++ windowswebapp.tf
@@ -1,6 +1,5 @@
 resource "azurerm_service_plan" "windowservplan" {
-  for_each            = {for x in local.deployment: x=>x}
-  name                = each.key
+  name                = "example-windows"
   resource_group_name = azurerm_resource_group.batch06.name
   location            = azurerm_resource_group.batch06.location
   sku_name            = "P1v2"
@@ -11,16 +10,16 @@ resource "azurerm_windows_web_app" "windowswebbapp" {
   for_each            = {for x in local.deployment: x=>x}
   name                = each.key
   resource_group_name = azurerm_resource_group.batch06.name
-  location            = azurerm_service_plan.windowservplan[each.value].location
-  service_plan_id     = azurerm_service_plan.windowservplan[each.value].id
+  location            = azurerm_service_plan.windowservplan.location
+  service_plan_id     = azurerm_service_plan.windowservplan.id
 
   site_config {}
 }
 
 resource "azurerm_windows_web_app_slot" "windowsappslot" {
-  for_each       = {for x in local.deployment: x=>x}
+  for_each       = azurerm_windows_web_app.windowswebbapp
   name           = each.key
-  app_service_id = azurerm_windows_web_app.windowswebbapp[each.value].id
+  app_service_id = each.value.id
 
   site_config {}
 }
