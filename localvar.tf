77da24299d0d85e434aec021ff9ed8898de8298d
hashimq27 <hashim.qasim@outlook.com>
2023-10-05 20:55:11 -0400

    sdfasdfgas

--- localvar.tf
+++ localvar.tf
@@ -3,7 +3,7 @@ locals {
     deployment   =["Val1","Val2", "Val3", "Val4", "Val5", "Val6", "Val7", "Val8", "Val9", "Val10"]
     gateways     =["gateway1", "gateway2", "gateway3", "gateway4", "gateway5"]
     networks     =["network1", "network2", "network3", "network4", "network5"]
-    linux_app=[for f in fileset("${path.module}/yaml", "[^_]*.yaml") : yamldecode(file("${path.module}//${f}"))]
+    linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/yaml/${f}"))]
     linux_app_list = flatten([
     for app in local.linux_app : [
       for linuxapps in try(app.listoflinuxapp, []) :{
@@ -13,7 +13,7 @@ locals {
               }
     ]
 ])
-    win_app=[for f in fileset("${path.module}/yaml", "[^_]*.yaml") : yamldecode(file("${path.module}//${f}"))]
+    linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/yaml/${f}"))]
     win_app_list = flatten([
     for app in local.win_app : [
       for winapps in try(app.listofwinapp, []) :{
