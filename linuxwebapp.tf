d020f57c26103c1561064740e382ef3a9ed2dee5
hashimq27 <hashim.qasim@outlook.com>
2023-10-05 20:19:54 -0400

    eqrseafas

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -8,8 +8,8 @@ resource "azurerm_service_plan" "servplan" {
 }
 
 resource "azurerm_linux_web_app" "linwebapp" {
-  for_each            = {for x in local.deployment: x=> x}
-  name                = each.key
+  for_each            = azurerm_service_plan.servplan
+  name                = each.value.name
   resource_group_name = azurerm_resource_group.batch06.name
   location            = azurerm_service_plan.servplan.location
   service_plan_id     = azurerm_service_plan.servplan.id
