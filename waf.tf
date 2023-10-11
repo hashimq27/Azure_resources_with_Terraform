9fffb5670c856c08f97f67e104f3b9c4384f18c9
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-10-11 00:30:38 -0400

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -1,18 +1,19 @@
 resource "azurerm_web_application_firewall_policy" "wafpol" {
-  name                = "example"
+  for_each            = {for policy in local.waf_policy_list: "${policy.name}" => policy}
+  name                = each.value.name
   resource_group_name = azurerm_resource_group.batch06.name
   location            = azurerm_resource_group.batch06.location
 
  managed_rules {
     managed_rule_set {
-      version = "3.2"
+      version = each.value.version
       rule_group_override {
-        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
+        rule_group_name = each.value.rule_group_name
 
         rule {
-          id      = "920440"
-          enabled = true
-          action  = "Block"
+          id      = each.value.id
+          enabled = each.value.enabled
+          action  = each.value.action
         }
       }
     }
