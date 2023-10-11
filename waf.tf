fe9b226612617fdca04f9ab025fbab7662532350
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-10-10 23:59:58 -0400

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -20,7 +20,7 @@ resource "azurerm_web_application_firewall_policy" "wafpol" {
     }
     action = each.value.action
 
-   managed_rules {
+  managed_rules {
     exclusion {
       match_variable          = "RequestHeaderNames"
       selector                = "x-company-secret-header"
